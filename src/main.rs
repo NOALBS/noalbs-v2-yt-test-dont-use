d5edae93fb1dab492245942948e67726b26866ea
b3ck <1740542+b3ck@users.noreply.github.com>
2024-08-06 01:30:58 -0500

    Update main.rs
    testing youtube stuffs...

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use tokio::signal;
 use noalbs::{chat::ChatPlatform, config, Noalbs};
 use tracing::warn;
 
+mod chat; // Ensure this points to the correct module path
+
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv::dotenv().ok();
@@ -89,8 +91,9 @@ async fn main() -> Result<()> {
     }
 
     if env::var("YOUTUBE_CHANNEL_ID").is_ok() {
-        let youtube = noalbs::chat::YouTube::new(chat_tx.clone()).await?;
-        youtube.start().await?;
+        let yt_channel_id = env::var("YOUTUBE_CHANNEL_ID")?;
+        let youtube = chat::youtube::YoutubeChat::new(yt_channel_id, chat_tx.clone());
+        youtube.start().await;
         chat_handler.add_chat_sender(ChatPlatform::Youtube, Arc::new(youtube));
     }
 
