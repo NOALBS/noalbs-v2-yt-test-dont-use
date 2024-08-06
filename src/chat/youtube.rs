b20219d7b60bbe5f6a81a970f806be0e3d74b0fd
b3ck <1740542+b3ck@users.noreply.github.com>
2024-08-05 21:29:53 -0500

    Update youtube.rs
    testing youtube stuffs..

--- src/chat/youtube.rs
+++ src/chat/youtube.rs
@@ -1,22 +1,25 @@
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
 use tokio::{task, time};
-use tracing::{info, error, debug};
-use youtube_chat::LiveChat;
+use tracing::{info, debug, error};
+use youtube_chat::LiveChatClientBuilder;
 use tokio_stream::StreamExt;
 use crate::chat::{self, ChatPlatform, HandleMessage, ChatSender};
 
 pub struct YouTube {
-    live_chat: LiveChat,
+    live_chat: Arc<Mutex<LiveChatClientBuilder>>,
     chat_handler_tx: ChatSender,
 }
 
 impl YouTube {
     pub async fn new(chat_handler_tx: ChatSender) -> Result<Self, Box<dyn std::error::Error>> {
         let yt_channel_id = std::env::var("YOUTUBE_CHANNEL_ID")?;
-        let live_chat = LiveChat::new(yt_channel_id).await?;
+        let live_chat = LiveChatClientBuilder::new()
+            .channel_id(yt_channel_id)
+            .build();
+
         Ok(Self {
-            live_chat,
+            live_chat: Arc::new(Mutex::new(live_chat)),
             chat_handler_tx,
         })
     }
@@ -29,19 +32,32 @@ impl YouTube {
         let live_chat = self.live_chat.clone();
 
         let chat_handle = task::spawn(async move {
-            let mut chat_stream = live_chat.chat_stream();
-            while let Some(chat_item) = chat_stream.next().await {
-                if *process_messages.lock().unwrap() {
-                    debug!("{}: {}", chat_item.author_name, chat_item.message);
-                    let message = HandleMessage::ChatMessage(chat::ChatMessage {
-                        platform: ChatPlatform::YouTube,
-                        permission: chat::Permission::Public,
-                        channel: chat_item.author_name.clone(),
-                        sender: chat_item.author_name.clone(),
-                        message: chat_item.message.clone(),
-                    });
-                    chat_handler_tx.send(message).await.unwrap();
-                }
+            let mut live_chat = live_chat.lock().unwrap();
+            live_chat
+                .on_chat(move |chat_item| {
+                    if *process_messages.lock().unwrap() {
+                        debug!("{}: {}", chat_item.author_name, chat_item.message);
+                        let message = HandleMessage::ChatMessage(chat::ChatMessage {
+                            platform: ChatPlatform::Youtube,
+                            permission: chat::Permission::Public,
+                            channel: chat_item.author_name.clone(),
+                            sender: chat_item.author_name.clone(),
+                            message: chat_item.message.clone(),
+                        });
+                        chat_handler_tx.send(message).await.unwrap();
+                    }
+                })
+                .on_error(|err| {
+                    error!("YouTube chat error: {:?}", err);
+                })
+                .start()
+                .await
+                .unwrap();
+
+            let mut interval = time::interval(Duration::from_millis(3000));
+            loop {
+                interval.tick().await;
+                live_chat.execute().await;
             }
         });
 
